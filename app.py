4f73eef6a078007f9d24b61362626976f0e319ae
Sangamithran <sangamithran97@gmail.com>
2025-05-27 13:25:08 +0530

    Updated PostgrSQL and app.py

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask, request, jsonify
+import psycopg2
 
 app= Flask(__name__)
+
+conn= psycopg2.connect(
+	host="localhost:,
+	database="carzchatbot",
+	user="postgres",
+	password="postgres97",
+	port="5432"
+)
+cursor=conn.cursor()	
 
 @app.route('/')
 def home():
@@ -14,13 +24,26 @@ def webhook():
 
     if intent == 'car-info':
         car_name= req.get('queryResult').get('parameters').get('carname')
-        
-        response_text= f"{car_name} is a sedan with petrol engine, manual transmission, and comes in red, blue, or black. The on-road price of {car_name} is Rs. 10 Lakhs, with Rs. 1.5 Lakhs tax, total Rs. 11.5 Lakhs."
-    
-    else:
-        response_text="Sorry, I didn't understand that."
-    
-    return jsonify({"fulfillmentText":response_text})
+	cursor.execute("SELECT * FROM car_info WHERE car_name ILIKE %s", (f"%{car_name}%",))
+	result= cursor.fetchone()
+
+	if result:
+		brand, name, type, fuel_type, transmission, variants, price, color = result
+		reply= (
+			f"**{car_name}** by **{brand}** is a {type} car.\n"
+			f"- Fuel Types: {fuel_type}
+			f"- Transmission: {transmission}\n"
+			f"- Variants: {variants}\n"
+			f"- On-road price: {onroad_price}\n"
+			f"- Available Colors: {color}"
+		)
+       
+    	else:
+        	reply="Sorry, I couldn't find that car in my database."
+  
+	return jsonify({"fulfillmentText":reply})
+  
+    return jsonify({"fulfillmentText":"Sorry, I didn't understand that})
 
 if __name__ == '__main__':
     app.run()
