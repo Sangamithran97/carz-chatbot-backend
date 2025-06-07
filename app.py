a2f3dc806f1b36a4223d23998ba36f3d48975f61
Sangamithran97 <sangamithran97@gmail.com>
2025-06-07 14:16:51 +0530

    Fix Indentation errors

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 from flask import Flask, request, jsonify
 import psycopg2
 
-app= Flask(__name__)
+app = Flask(__name__)
 
-conn= psycopg2.connect(
-	host="localhost",
-	database="carzchatbot",
-	user="postgres",
-	password="postgres97",
-	port="5432"
+conn = psycopg2.connect(
+    host="localhost",
+    database="carzchatbot",
+    user="postgres",
+    password="postgres97",
+    port="5432"
 )
-cursor=conn.cursor()	
+cursor = conn.cursor()
 
 @app.route('/')
 def home():
@@ -18,32 +18,31 @@ def home():
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    req= request.get_json(force=True)
+    req = request.get_json(force=True)
 
-    intent= req.get('queryResult',{}).get('intent',{}).get('displayName')
+    intent = req.get('queryResult', {}).get('intent', {}).get('displayName')
 
     if intent == 'car-info':
-        car_name= req.get('queryResult').get('parameters').get('carname')
-	cursor.execute("SELECT * FROM car_info WHERE car_name ILIKE %s", (f"%{car_name}%",))
-	result= cursor.fetchone()
+        car_name = req.get('queryResult').get('parameters').get('carname')
+        cursor.execute("SELECT * FROM car_info WHERE car_name ILIKE %s", (f"%{car_name}%",))
+        result = cursor.fetchone()
 
-	if result:
-		brand, name, type, fuel_type, transmission, variants, price, color = result
-		reply= (
-			f"**{car_name}** by **{brand}** is a {type} car.\n"
-			f"- Fuel Types: {fuel_type}
-			f"- Transmission: {transmission}\n"
-			f"- Variants: {variants}\n"
-			f"- On-road price: {onroad_price}\n"
-			f"- Available Colors: {color}"
-		)
-       
-    	else:
-        	reply="Sorry, I couldn't find that car in my database."
-  
-	return jsonify({"fulfillmentText":reply})
-  
-    return jsonify({"fulfillmentText":"Sorry, I didn't understand that})
+        if result:
+            brand, name, type_, fuel_type, transmission, variants, onroad_price, color = result
+            reply = (
+                f"**{car_name}** by **{brand}** is a {type_} car.\n"
+                f"- Fuel Types: {fuel_type}\n"
+                f"- Transmission: {transmission}\n"
+                f"- Variants: {variants}\n"
+                f"- On-road price: {onroad_price}\n"
+                f"- Available Colors: {color}"
+            )
+        else:
+            reply = "Sorry, I couldn't find that car in my database."
+
+        return jsonify({"fulfillmentText": reply})
+
+    return jsonify({"fulfillmentText": "Sorry, I didn't understand that."})
 
 if __name__ == '__main__':
     app.run()
